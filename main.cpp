b41d2a29151dc691cac74666ee97485099bccd34
pyrotnak41 <pyrotank41@gmail.com>
2019-09-30 01:33:11 -0400

    added case 5 and 6 successfully*

--- main.cpp
+++ main.cpp
@@ -31,8 +31,39 @@ void displayIdInfo()
          << " \n";
 }//end displayIDInfo()
 
-// simple function to open a file and return the stream -------------------------------------
-ifstream openTextFile( string nameOfFile = " " ){
+//binarySearch() starts here ----------------------------------------------------------------
+int binarySearch(string searchWord, vector< string> dictionary){
+
+    int low, mid, high;     // array indices for binary search
+    int searchResult = -1;  // Stores index of word if search succeeded, else -1
+
+    // Binary search for word
+    low = 0;
+    high = dictionary.size() - 1;
+    while ( low <= high)  {
+        mid = (low + high) / 2;
+        // SearchResult negative value means word is to the left, positive value means
+        // word is to the right, value of 0 means word was found
+        searchResult = searchWord.compare( dictionary[ mid] );
+        if ( searchResult == 0)  {
+            // Word IS in dictionary, so return the index where the word was found
+            return mid;
+        }
+        else if (searchResult < 0)  {
+            high = mid - 1; // word should be located before the mid location
+        }
+        else  {
+            low = mid + 1; // word should be located after the mid location
+        }
+    }
+
+    // Word was not found
+    return -1;
+}//end binarySearch()
+
+
+// Storing all the words in a words in a vector ----------------------------------------------
+void storeWordsInVector(vector<string>& dictionary, string nameOfFile){
   ifstream inputFileStream;  // Declare the input file stream
 
   // Open input file
@@ -50,15 +81,13 @@ ifstream openTextFile( string nameOfFile = " " ){
       exit( -1);
   }
 
-  return inputFileStream;
-}
-
-// Storing all the words in a words in a vector ----------------------------------------------
-void storeWordsInVector(vector<string>& dictionary, string nameOfFile){
-  ifstream inputFileStream = openTextFile(nameOfFile);
   string line;
   int count = 0;
   while(getline(inputFileStream, line)){
+    // checking and changing word for capital case word to lowercase.
+    if(isupper(line.at(0))){
+      line.at(0) = tolower(line.at(0));
+    }
     dictionary.push_back(line);
   }
   inputFileStream.close();
@@ -102,48 +131,98 @@ void printQuantity(vector<int> wordOccuranceQuantity){
   // iterating through wordOccuranceQuantity vector to print words length occurance that is greater than 1400
   for (int i = 0; i < wordOccuranceQuantity.size() - 1; i++){
     if(wordOccuranceQuantity.at(i) > 1400){
-      cout << " "
-           << setw(4)
-           << i+1
-           << "    ";
-
-      cout << setw(6)
-           << wordOccuranceQuantity.at(i) << endl;
+      cout << " " << setw(4) << i+1 << "    ";
+      cout << setw(6) << wordOccuranceQuantity.at(i) << endl;
     }
   }
 
   cout << endl;
 } // printQuantity() ends here
 
-long binarySearch(string searchWord, vector< string> dictionary){
 
-    long low, mid, high;     // array indices for binary search
-    long searchResult = -1;  // Stores index of word if search succeeded, else -1
+vector<string> findSequence(string startWord, string endWord,vector<string> dictionary, int debug = 0){
 
-    // Binary search for word
-    low = 0;
-    high = dictionary.size() - 1;
-    while ( low <= high)  {
-        mid = (low + high) / 2;
-        // SearchResult negative value means word is to the left, positive value means
-        // word is to the right, value of 0 means word was found
-        searchResult = searchWord.compare( dictionary[ mid] );
-        if ( searchResult == 0)  {
-            // Word IS in dictionary, so return the index where the word was found
-            return mid;
+  vector<string> tempVector; // FIXME, put me in main() later
+  tempVector.push_back(startWord);
+  string tempString;
+
+  int startIndex = 0;
+
+  // think of an explaination here.... lol.. this one just makes senses future karan, dont be lazy and try to understand y this loop ... lmao.
+  for(int i = 0; i < tempVector.size(); i++ ){
+    // itterating through all the indexes for of the current word to find possible match in dictionary.
+    for(int index = 0; index < startWord.length(); index++ ){
+      // itterating through all the possible combination for changing only one character at a time
+      for(char c = 'a'; c <= 'z'; c++ ){
+        // skipping repetation of word, ex if the starting word was dog, c = 'd', then we will skip dog.
+        if(tempVector.at(i).at(index) == c){
+          continue;
         }
-        else if (searchResult < 0)  {
-            high = mid - 1; // word should be located before the mid location
+
+        tempString = tempVector.at(i);
+        tempString.at(index) = c;
+
+        if( binarySearch(tempString, dictionary) != -1){
+
+          bool wordFound = false;
+          
+          // simple search algorithm to make sure if any
+          for (int sIndex = 0; sIndex < tempVector.size(); sIndex++){
+            if(tempString == tempVector.at(sIndex)){
+              wordFound = true;
+            }
+          }// simple search algo ends here.
+
+          if(!wordFound){ // if the word was not found push the string in the vector
+              tempVector.push_back(tempString);
+          }
+        }// binary search in dictionary ends here.
+      }// char iteration one word ends here.
+    }// index iteration ends here.
+
+
+    if(debug == 1){ // debug mode on
+
+      cout << i << ".  " << tempVector.at(i)<< ":    ";
+
+      for(int j = startIndex + 1; j < tempVector.size(); j++){
+        cout << j << ":"<<tempVector.at(j)<< " ";
+
+        // if endword is found then break out of this function
+        if(tempVector.at(j) == endWord){
+          cout << endl << "\nWinning sequence was found!" << endl << endl;
+          return tempVector;
         }
-        else  {
-            low = mid + 1; // word should be located after the mid location
+      }
+
+      cout << endl;
+    }
+    else if( debug == 2){
+      for(int j = startIndex + 1; j < tempVector.size(); j++){
+        // if endword is found then break out of this function
+        if(tempVector.at(j) == endWord){
+          cout << endl << "\nWinning sequence was found!" << endl << endl;
+          return tempVector;
         }
+      }
     }
 
-    // Word was not found
-    return -1;
-}//end binarySearch()
+    // start index is set to current tempVector size-1, so in next print cycle
+    // it starts from previously left position i.e non printed terms.
+    startIndex = tempVector.size()-1;
+  }
 
+}//findSequence() ends here.
+
+//displayAnsSeq starts here ----------------------------------------------------------------------
+void displayAnsSeq(vector<string> sequenceVector){
+
+  for(int i = 0; i < sequenceVector.at(0).size(); i++){
+
+  }
+
+
+}//displayAnsSeq() ends here.
 
 // main() starts here ----------------------------------------------------------------------------
 int main() {
@@ -207,13 +286,17 @@ int main() {
         case 4:
             break;
 
-        case 5:
+        case 5: //Find the end word with debug
+            findSequence(startWord, endWord, dictionary, 1);
             break;
 
-        case 6:
+        case 6: //Find the end word WITHOUT debug but success.
+            findSequence(startWord, endWord, dictionary, 2);
             break;
 
-        case 7:
+        case 7: //Display an answer sequence
+            vector<string> sequenceVector = findSequence(startWord, endWord, dictionary);
+            displayAnsSeq(sequence);
             break;
 
         case 8: //8. Exit the program
@@ -221,10 +304,9 @@ int main() {
             exit(-1); // exit the program
             break;
 
-        default: // invalud responce
+        default: // invalid responce
           while(1){ // waiting for the user to input a right input
              cout << "invalid response! try again!!" << endl; // if user choice is in the range
-
              cout << "Your choice -> ";
              cin  >> userChoice;
              cout << endl;
@@ -234,10 +316,5 @@ int main() {
           }
       }
   }
-
-
-
-
-
   return 0;
 }
